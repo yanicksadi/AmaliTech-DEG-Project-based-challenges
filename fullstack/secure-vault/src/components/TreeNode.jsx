794da5410383e376e4bf90f6e77460b0c856b83e
Ishimwe Muhire Sadi <yanicksadi10@gmail.com>
2026-04-26 22:59:11 +0200

    Update TreeNode.jsx to add auto expand folders when searching

--- fullstack/secure-vault/src/components/TreeNode.jsx
+++ fullstack/secure-vault/src/components/TreeNode.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const TreeNode = ({ node, onSelect, level = 0 }) => {
+const TreeNode = ({ node, onSelect, level = 0, search }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const isFolder = node.type === "folder";
+
+  useEffect(() => {
+    if (search) {
+      setIsOpen(true);
+    }
+  }, [search]);
 
   const handleClick = () => {
     if (isFolder) {
@@ -18,7 +24,7 @@ return (
     <div onClick={handleClick} style={{cursor: "pointer", padding: "6px", borderRadius: "4px" }}> 
       {isFolder ? (isOpen ? "folder" : "folder") : "file"}{node.name}
     </div>
-    {isFolder && isOpen && node.children?.map((child) => (<TreeNode key={child.id} node={child} onselect={onSelect} level={level + 1}/>))}
+    {isFolder && isOpen && node.children?.map((child) => (<TreeNode key={child.id} node={child} onselect={onSelect} level={level + 1} search={search}/>))}
   </div>
   );  
 };
