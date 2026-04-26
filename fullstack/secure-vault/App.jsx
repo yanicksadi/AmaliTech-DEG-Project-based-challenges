115214b3f3807265c28b0fb229ba4783a4395d21
Ishimwe Muhire Sadi <yanicksadi10@gmail.com>
2026-04-26 03:55:37 +0200

    setup basic layout structure

--- fullstack/secure-vault/App.jsx
+++ fullstack/secure-vault/App.jsx
@@ -1,5 +1,11 @@
 function App(){
-  return <h1>SecureVault Dashboard</h1>
+  return (
+<div>
+    <h1>SecureVault Dashboard</h1>
+    <div>Layout coming</div>
+  </div>
+  );
+  
 }
 
 export default App;
