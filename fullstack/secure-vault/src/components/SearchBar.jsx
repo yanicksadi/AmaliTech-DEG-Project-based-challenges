0c291795b0085ab0bf1710510e4ced1595aa31a0
Ishimwe Muhire Sadi <yanicksadi10@gmail.com>
2026-04-26 04:04:53 +0200

    Implement recursive TreeNode logic and Search Bar

---
+++ fullstack/secure-vault/src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+const searchBar = ({ onSearch }) => {
+  return (
+    <input type="text" placeholder="search files ..." onChange={(e) => onSearch(e.target.value)} style={{ width: "100%", padding: "10px", background: "#111827", color: "#fff", border: "1px solid #374151", borderRadius: "6px"
+
+     }}
+    />
+  );
+};
+export default SearchBar;
